ed8dcbd4d4515ba498596e50e0305b1da01e499b
4E6IR <93246868+4E6IR@users.noreply.github.com>
2025-08-27 09:38:02 +0500

    Create Get-HypervisorName-From-VM — копия.ps1
    Проверка добавления файла в репозиторий

---
+++ Get-HypervisorName-From-VM — копия.ps1
@@ -0,0 +1,4 @@
+﻿Invoke-Command -ComputerName rws-probe-01.railsystems.kz -ScriptBlock {Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Virtual Machine\Guest\Parameters" -Name HostName} -Credential(Get-Credential) | fl Hostname
+#
+#
+#
