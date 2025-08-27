802e8e2292a9785a05bdb81a98b4bda3ed1cd4e3
4E6IR <93246868+4E6IR@users.noreply.github.com>
2025-08-27 09:42:32 +0500

    Удаление лишних строк Удаление тестового файла

--- Get-HypervisorName-From-VM.ps1
+++ Get-HypervisorName-From-VM.ps1
@@ -1,4 +1,1 @@
 ﻿Invoke-Command -ComputerName rws-probe-01.railsystems.kz -ScriptBlock {Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Virtual Machine\Guest\Parameters" -Name HostName} -Credential(Get-Credential) | fl Hostname
-#
-#
-#
