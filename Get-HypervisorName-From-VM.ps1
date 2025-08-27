35d6955e01031f1675833344cd4d713d11563207
4E6IR <93246868+4E6IR@users.noreply.github.com>
2025-08-27 09:34:54 +0500

    Update Get-HypervisorName-From-VM.ps1

--- Get-HypervisorName-From-VM.ps1
+++ Get-HypervisorName-From-VM.ps1
@@ -1,1 +1,2 @@
 ﻿Invoke-Command -ComputerName rws-probe-01.railsystems.kz -ScriptBlock {Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Virtual Machine\Guest\Parameters" -Name HostName} -Credential(Get-Credential) | fl Hostname
+ #
